8ce7bab700751c60eba812514f2d723eb8785c59
Henrique <henrique11rodrigues@gmail.com>
2022-12-07 21:27:34 -0300

    Adding Mock Data

---
+++ Tests/MockData/TestMockData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MinimalApi.Models;
+
+namespace Tests.MockData
+{
+    internal class TestMockData
+    {
+        public static List<Todo> GetTodos()
+        {
+            return new List<Todo>{
+             new Todo{
+                 Id = 1,
+                 Name = "Need To Go Shopping",
+                 IsComplete = true
+             },
+             new Todo{
+                 Id = 2,
+                 Name = "Cook Food",
+                 IsComplete = true
+             },
+             new Todo{
+                 Id = 3,
+                 Name = "Play Games",
+                 IsComplete = false
+             }
+         };
+        }
+    }
+}
