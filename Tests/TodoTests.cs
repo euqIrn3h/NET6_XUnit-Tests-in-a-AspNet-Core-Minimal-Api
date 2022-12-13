1876d48fb3877eae3e4dd23b4a6f3796caf5257d
Henrique <henrique11rodrigues@gmail.com>
2022-12-13 16:50:31 -0300

    Adding Todo tests
    Adding a create test and a Validation created test

---
+++ Tests/TodoTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http.Json;
+using MinimalApi.Models;
+using System.Net;
+
+namespace Tests
+{
+    public class TodoTests
+    {
+        [Fact]
+        public async Task Create_Todo()
+        {
+            await using var application = new WebApplicationFactory<Program>();
+
+            var client = application.CreateClient();
+
+            var result = await client.PostAsJsonAsync("/todo", new Todo
+                {
+                    Name = "Name",
+                    IsComplete = true
+                }
+            );
+
+            Assert.Equal(HttpStatusCode.Created, result.StatusCode);
+
+        }
+
+        [Fact]
+        public async Task Create_Todo_Validate_Object()
+        {
+            await using var application = new WebApplicationFactory<Program>();
+
+            var client = application.CreateClient();
+
+            var result = await client.PostAsJsonAsync("/todo", new Todo());
+
+            Assert.Equal(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+    }
+}
