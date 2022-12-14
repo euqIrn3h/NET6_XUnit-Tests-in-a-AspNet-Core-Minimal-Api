33e6c21945898c5fb93f09f450cf22cc86661ea7
Henrique <henrique11rodrigues@gmail.com>
2022-12-14 12:57:03 -0300

    refactoring the code and adding empty list test

--- Tests/TodoTests.cs
+++ Tests/TodoTests.cs
@@ -13,14 +13,20 @@ namespace Tests
 {
     public class TodoTests
     {
+
+        private WebApplicationFactory<Program> App { get; set; }
+        private System.Net.Http.HttpClient Client { get; set; }
+
+        public TodoTests()
+        {
+            App = new WebApplicationFactory<Program>();
+            Client = App.CreateClient();
+        }
+
         [Fact]
         public async Task Create_Todo()
         {
-            await using var application = new WebApplicationFactory<Program>();
-
-            var client = application.CreateClient();
-
-            var result = await client.PostAsJsonAsync("/todo", new Todo
+            var result = await Client.PostAsJsonAsync("/todo", new Todo
                 {
                     Name = "Name",
                     IsComplete = true
@@ -28,19 +34,24 @@ namespace Tests
             );
 
             Assert.Equal(HttpStatusCode.Created, result.StatusCode);
+        }
 
+        [Fact]
+        public async Task Get_Empty_Todo_List()
+        {
+            var result = await Client.GetFromJsonAsync<List<Todo>>("/todo");
+
+            Assert.NotNull(result);
+            Assert.Equal(0, result.Count);
         }
 
         [Fact]
         public async Task Create_Todo_Validate_Object()
         {
-            await using var application = new WebApplicationFactory<Program>();
-
-            var client = application.CreateClient();
-
-            var result = await client.PostAsJsonAsync("/todo", new Todo());
+            var result = await Client.PostAsJsonAsync("/todo", new Todo());
 
             Assert.Equal(HttpStatusCode.BadRequest, result.StatusCode);
         }
+
     }
 }
